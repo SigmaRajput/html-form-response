50cb5b6170b4cb8e51836f7f9aa7e1230b340fbb
SigmaRajput <Ravisinghrajputana9@gmail.com>
2023-09-29 10:50:26 +0530

    test

---
+++ process.php
@@ -0,0 +1,30 @@
+<?php
+require 'PHPMailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+// SMTP settings for Gmail
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'ravi.roxxx.0001@gmail.com'; // Your Gmail email address
+// $mail->Password = 'your_'; // Your Gmail password
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+
+// Sender and recipient
+$mail->setFrom($_POST['email'], $_POST['name']);
+$mail->addAddress('ravi.roxxx.0001@gmail.com'); // Your Gmail email address
+
+// Email content
+$mail->isHTML(false);
+$mail->Subject = 'Contact Form Submission';
+$mail->Body = $_POST['message'];
+
+// Send the email
+if ($mail->send()) {
+    echo 'Message sent!';
+} else {
+    echo 'Error: ' . $mail->ErrorInfo;
+}
+?>
